1e94e95afcc18592fc0b8474e32f5e91b36e3ba6
paolo-mangraviti-itimonaco-it <80854427+paolo-mangraviti-itimonaco-it@users.noreply.github.com>
2024-05-13 13:39:18 +0200

    Create operazioni.php
    .

---
+++ operazioni.php
@@ -0,0 +1,14 @@
+<html lang="it">
+	<head>
+		<title>Elenco Operazioni</title>
+	</head>
+	<body>
+		<h1>Elenco Operazioni</h1>
+		<ol>
+			<li><a href="formInserimento.php">(Create) Inserimento</a></li>
+			<li><a href="formRicerca-visualizzazione.php">(Read) Visualizzazione</a></li>
+			<li><a href="formRicerca-modifica.php">(Update) Modifica</a></li>
+			<li><a href="formRicerca-cancellazione.php">(Delete) Cancellazione</a></li>
+		</ol>
+	</body>
+</html>
