3e65bbfc7750efed64f8c4a4d7f068e2de49424f
paolo-mangraviti-itimonaco-it <80854427+paolo-mangraviti-itimonaco-it@users.noreply.github.com>
2024-05-13 14:05:00 +0200

    Update formInserimento.php

--- formInserimento.php
+++ formInserimento.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html lang="it">
 	<head>
 		<title>Form Inserimento</title>
@@ -17,7 +18,7 @@
 			<fieldset>
 				<legend>Dati Descrittivi</legend>
 				<input type="radio" name="r1" id="r1_1" value="concerto" checked><label for="r1_1">Cocerto</label><br>
-				<input type="radio" name="r1"  id="r1_2"value="teatro"><label for="r1_2">Teatro</label><br>
+				<input type="radio" name="r1" id="r1_2" value="teatro"><label for="r1_2">Teatro</label><br>
 				<input type="radio" name="r1" id="r1_3" value="sport"><label for="r1_3">Sport</label>
 				<br><br>
 				<input type="checkbox" value="libero" name="r3[]" id="r3_1"><label for="r3_1">Libero</label><br>
