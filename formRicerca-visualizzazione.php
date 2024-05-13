e8359ee168c4e6835badb84e16dee0441d786c20
paolo-mangraviti-itimonaco-it <80854427+paolo-mangraviti-itimonaco-it@users.noreply.github.com>
2024-05-13 14:14:43 +0200

    Update formRicerca-visualizzazione.php

--- formRicerca-visualizzazione.php
+++ formRicerca-visualizzazione.php
@@ -10,13 +10,14 @@
 				<legend>Dati ricerca</legend>
 				<label for="titoloEvento"></label>
 				<input type="text" name="titoloEvento" id="titoloEvento">
-      </fieldset>
-      <fieldset>
+				</fieldset>
+				<fieldset>
 				<legend>Tipo evento</legend>
 				<input type="radio" name="r1" id="r1_1" value="concerto" checked><label for="r1_1">Cocerto</label><br>
 				<input type="radio" name="r1" id="r1_2" value="teatro"><label for="r1_2">Teatro</label><br>
 				<input type="radio" name="r1" id="r1_3" value="sport"><label for="r1_3">Sport</label>
-      </fieldset>
+			</fieldset>
+			<input type="submit" value="Cerca">
 		</form>
 	</body>
 </html>
