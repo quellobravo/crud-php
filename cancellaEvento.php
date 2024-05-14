167d0100da47e5c3eb68a405e38e4ca0a950ba4c
paolo-mangraviti-itimonaco-it <80854427+paolo-mangraviti-itimonaco-it@users.noreply.github.com>
2024-05-14 20:32:40 +0200

    Create cancellaEvento.php

---
+++ cancellaEvento.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+    <title>Cancellazione</title>
+  </head>
+  <body>
+    <h1>Cancellazione</h1>
+    <?php
+		$serverName = "localhost";
+		$IPserver = "127.0.1";
+		$username = "applicazioneWeb";
+		$password = "123456_Web&&";
+		$db = "listaeventi";
+
+		// Stabilisce la connessione al DBMS remoto
+		$connessione = mysqli_connect($serverName, $username, $password, $db);
+
+		// Check connection
+		if (!$connessione) { die("Errore connessione");	}
+
+		$istruzioneSQL = "DELETE FROM eventi WHERE ";
+		$istruzioneSQL .= "id = ".$_GET['id'].";";
+		
+		echo("<p>".$istruzioneSQL."</p>");
+		
+		mysqli_query($connessione,$istruzioneSQL);
+
+		mysqli_close($connessione);
+    ?> 	
+  
+  </body>
+</html>
