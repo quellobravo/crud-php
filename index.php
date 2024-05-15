bc34e1d66b5479f84158b75a8edcebf1a98c9141
paolo-mangraviti-itimonaco-it <80854427+paolo-mangraviti-itimonaco-it@users.noreply.github.com>
2024-05-15 09:12:44 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="it">
+	<head>
+		<title>Elenco Operazioni</title>
+	</head>
+	<body>
+		<h1>Elenco Operazioni</h1>
+		<ol>
+			<li><a href="formInserimento.php">(Create) Inserimento Evento</a></li>
+			<li><a href="formRicerca.php">(Read) Visualizzazione Evento</a></li>
+			<li><a href="ricercaMassiva.php">(Read) Visualizzazione (tutti i dati)</a></li>
+			<li><a href="formRicercaPreModifica.php">(Update) Modifica Evento</a></li>
+			<li><a href="formRicercaPreCancella.php">(Delete) Cancellazione Evento</a></li>
+		</ol>
+	</body>
+</html>
